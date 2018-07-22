dfb6b8446b14fa9616d645e12299bcc92a935ab0
Matt Dolan <mdolan.code@gmail.com>
2018-07-22 08:03:49 -0700

    First Repo

---
+++ object.js
@@ -0,0 +1,20 @@
+//Write code to retrieve "Malfoy" from someObject
+//Go one "layer" at a time
+
+var  someObject = {
+  friends: [
+    {name: "Malfoy"},
+    {name: "Crabbe"},
+    {name: "Goyle"}
+  ],
+  color: "baby blue",
+  isEvil: "true"
+};
+
+
+//answer one "layer" at a time
+someObject.friends
+
+someObject.friends[0]
+
+someObject.friends[0].name
